8bc03edfbc708a0d11b92e9b4cf472fce3d97c30
Benjamin Longearet <firehist@gmail.com>
2018-05-24 10:24:32 +0200

    Add solutions

--- src/08-about-decorators/test.ts
+++ src/08-about-decorators/test.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai';
 var expect = chai.expect;
 
-import * as decorator from './decorators/log';
+import { logConstructor, logMethod } from './decorators/log';
 
 describe('about decorators', () => {
   var calculator: Calculator,
@@ -9,10 +9,10 @@ describe('about decorators', () => {
 
   console.info = (...args: any[]) => logStack.push(...args);
 
-  @decorator.logConstructor
+  @logConstructor
   class Calculator {
-    @decorator.logMethod
-    double(n: number) {
+    @logMethod
+    public double(n: number): number {
       return n * 2;
     }
   }
@@ -20,11 +20,21 @@ describe('about decorators', () => {
   beforeEach(() => calculator = new Calculator());
 
   it('1-should log constructor', () => {
+    /** Ennoncé
     expect(logStack).to.eql( _ );
+     */
+    expect(logStack).to.eql(['Create constructor Calculator']);
   });
 
   it('2-should log methods', () => {
+    /** Ennoncé
     calculator.double(2);
     expect(logStack).to.eql([]);
+    */
+    calculator.double(2);
+    expect(logStack).to.eql([
+      'Create constructor Calculator',
+      "Call: double(2) => 4"
+    ]);
   });
 });
